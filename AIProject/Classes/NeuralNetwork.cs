e54a891dcd54803bc09d6d55007eb24a3bff41c8
slik margor <slikmargor@gmail.com>
2022-05-25 14:22:27 +0200

    <<Added basic constructor>>

--- AIProject/Classes/NeuralNetwork.cs
+++ AIProject/Classes/NeuralNetwork.cs
@@ -17,7 +17,7 @@ public class NeuralNetwork
     //Biases for every layer -> Biases[i][j] where i -> a layer number,
     //j -> a node number
     public decimal[][] Biases { get; set; }
-    
+
     //Weigths for every layer -> Weights[i][j][z] where i-> a layer number, 
     //j -> a node number in a layer i, z-> a node number in a layer i+1
     //If N is a number of layers in our network then -> Weights[z-1][][]
@@ -25,8 +25,21 @@ public class NeuralNetwork
 
     public NeuralNetwork(int[] layers)
     {
+        Values = new decimal[layers.Length][];
+        Biases = new decimal[layers.Length][];
+        Weights = new decimal[layers.Length][][];
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            Values[i] = new decimal[layers[i]];
+            Biases[i] = new decimal[layers[i]];
+            Weights[i] = new decimal[layers[i]][];
+        }
+        for (int i = 0; i < layers.Length - 1; i++)
+            for (int j = 0; j < layers.Length; j++)
+            {
+                Weights[i][j] = new decimal[layers[i+1]];
+            }
 
     }
-
-
 }
