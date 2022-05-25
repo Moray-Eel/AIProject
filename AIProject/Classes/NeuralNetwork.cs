f31a434051843bd7fec4f20727c1d02312204153
slik margor <slikmargor@gmail.com>
2022-05-25 15:10:41 +0200

    <Changed decimal to double>

--- AIProject/Classes/NeuralNetwork.cs
+++ AIProject/Classes/NeuralNetwork.cs
@@ -10,35 +10,37 @@ namespace AIProject.Classes;
 public class NeuralNetwork
 {
 
+    private readonly Random _randomizer = new Random();
     //Values for every layer -> Values[i][j] where i -> a layer number,
     //j -> a node number
-    public decimal[][] Values { get; set; }
+    public double[][] Values { get; set; }
 
     //Biases for every layer -> Biases[i][j] where i -> a layer number,
     //j -> a node number
-    public decimal[][] Biases { get; set; }
+    public double[][] Biases { get; set; }
 
-    //Weigths for every layer -> Weights[i][j][z] where i-> a layer number, 
-    //j -> a node number in a layer i, z-> a node number in a layer i+1
-    //If N is a number of layers in our network then -> Weights[z-1][][]
-    public decimal[][][] Weights { get; set; }
+    //Weigths for every layer -> Weights[i][j][k] where i-> a layer number, 
+    //j -> a node number in a layer i, k-> a node number in a layer i+1
+    //If N is a number of layers in our network then -> Weights[N-1][][]
+    public double[][][] Weights { get; set; }
 
     public NeuralNetwork(int[] layers)
     {
-        Values = new decimal[layers.Length][];
-        Biases = new decimal[layers.Length][];
-        Weights = new decimal[layers.Length][][];
+
+        Values = new double[layers.Length][];
+        Biases = new double[layers.Length][];
+        Weights = new double[layers.Length][][];
 
         for (int i = 0; i < layers.Length; i++)
         {
-            Values[i] = new decimal[layers[i]];
-            Biases[i] = new decimal[layers[i]];
-            Weights[i] = new decimal[layers[i]][];
+            Values[i] = new double[layers[i]];
+            Biases[i] = new double[layers[i]];
+            Weights[i] = new double[layers[i]][];
         }
         for (int i = 0; i < layers.Length - 1; i++)
             for (int j = 0; j < layers.Length; j++)
             {
-                Weights[i][j] = new decimal[layers[i+1]];
+                Weights[i][j] = new double[layers[i+1]];
             }
 
     }
