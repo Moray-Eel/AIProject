19ef2ada7b0f3dab10a44978127fce52a2d584e3
slik margor <slikmargor@gmail.com>
2022-05-28 15:13:16 +0200

    <naprawiano bledy z tworzeniem tablicy>

--- AIProject/Classes/NeuralNetwork.cs
+++ AIProject/Classes/NeuralNetwork.cs
@@ -14,17 +14,17 @@ public class NeuralNetwork
 {
 
     private readonly Random _randomizer = new Random();
-    //Values for every layer -> Values[i][j] where i -> a layer number,
-    //j -> a node number
+    //Wartości dla każdej warstwy -> Values[i][j] gdzie i -> numer warstwy,
+    //j -> numer neuronu
     public double[][] Values { get; set; }
 
-    //Biases for every layer -> Biases[i][j] where i -> a layer number,
-    //j -> a node number
+    //Biasy dla kazdej warstwy -> Biases[i][j] gdzie i -> a numer warstwy,
+    //j -> numer neuronu
     public double[][] Biases { get; set; }
 
-    //Weigths for every layer -> Weights[i][j][k] where i-> a layer number, 
-    //j -> a node number in a layer i, k-> a node number in a layer i+1
-    //If N is a number of layers in our network then -> Weights[N-1][][]
+    //Waga dla każdej warstwy -> Weights[i][j][k] gdzie i-> numer warstwy, 
+    //j -> numer neuronu w warstwie i, k-> numer neuronu w warstwie i+1
+    //Jeśli nasza sieć ma N warstw to -> Weights[N-1][][]
     public double[][][] Weights { get; set; }
     
     public double LearningRate = 0.001;
@@ -40,29 +40,32 @@ public class NeuralNetwork
             Values[i] = new double[layers[i]];
             Biases[i] = new double[layers[i]];
         }
-        for (int i = 0; i < layers.Length - 1; i++)
+        for (int i = 0; i < layers.Length-1 ; i++)
         {
             Weights[i] = new double[layers[i]][];
             
-            for (int j = 0; j < layers.Length; j++)
+            for (int j = 0; j < Weights[i].Length; j++)
             {
                 Weights[i][j] = new double[layers[i+1]];
             }
         }
-    }
-    public void InitializeWeightsAndBiases()
+    }    /// <summary>
+    /// Inicjalizuje wagi i biasy w przedziale od min do max
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    public void InitializeWeightsAndBiases(int min, int max)
     {
         for (int i = 0; i < Weights.GetLength(0); i++)
-            for (int j = 0; j < Weights.GetLength(1); j++)
-                for (int k = 0; k < Weights.GetLength(2); k++)
+            for (int j = 0; j < Weights[i].Length; j++)
+                for (int k = 0; k < Weights[i][j].Length; k++)
                 {
-                    Weights[i][j][k] = _randomizer.NextDouble(-1, 1);
+                    Weights[i][j][k] = _randomizer.NextDouble(min, max);
                 }
     }
-  
 
     /// <summary>
-    /// Takes the array of values for input layer.
+    /// Przyjmuje tablicę wartości dla pierwszej warstwy
     /// </summary>
     /// <param name="input"></param>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
@@ -76,5 +79,14 @@ public class NeuralNetwork
             Values[0][i] = input[i];
         }
     }
+    public void Train()
+    {
+        for (int i = 1; i < Values.GetLength(0); i++)
+            for (int j=0; j < Weights[i].Length; j++)
+            {
+                Values[i][j] = Sum(Values[i - 1], Weights[i - 1]);
+            }
+    }
+    public double Sum(double[] values, double[][] weights) => values.Select((v, i) => v * weights[i][0]).Sum();
 
 }
--- AIProject/Program.cs
+++ AIProject/Program.cs
@@ -1,3 +1,5 @@
 ﻿using AIProject.Classes;
 
-NeuralNetwork xd = new(new int[] { 1, 2, 3 });
+NeuralNetwork xd = new(new int[] { 7, 7, 4 });
+xd.InitializeWeightsAndBiases(-1, 1);
+Console.WriteLine(xd);
