0366ee8f8c0e962bc0646b54dd87ee3f8f00b255
slik margor <slikmargor@gmail.com>
2022-06-22 15:37:55 +0200

    .

--- AIProject/Program.cs
+++ AIProject/Program.cs
@@ -8,9 +8,14 @@
 //Pliki moga byc w formacie tsv lub csv. Ilosci neuronow w 1 i ostatniej warstwie nie sa obliczane na podstawie wcytanych danych z pliku, tzn. jesli
 //mamy plik wejsciowy z 5 kolumnami danych to w 1 argumencie trzeba podac liczbe 5, tak samo jesli mamy 2 kolumny w pliku docelowym to trzeba podac w warstwie
 //wyjsciowej liczbe 2. Warstwy ukryte są tworzeone dynamicznie i można je modyfikować dowolnie
+//Trzeci argument to liczba epok
+//Czwarty argument to współczynnik podziału
+//Piąty argument to wartość boolowska określająca czy chcemy użyć funkcji ReLU w warstwach ukrytych
+//Szósty argument to zakres wartości losowych dla wag i biasów z zakresu < -wartosc,  wartosc >
+//Siódmy argument to wartość współczynnika uczenia
 
 
-NeuralNetwork neuralNetwork = new(new int[] { 4,3,5,1 }, new string[] { @"C:\Users\slikm\Pulpit\IrisInput.tsv", @"C:\Users\slikm\Pulpit\IrisTarget2.tsv" }, 3, 0.8 ,true, 1, 0.5);
+NeuralNetwork neuralNetwork = new(new int[] { 4,3,5,1 }, new string[] { @"C:\Users\slikm\Pulpit\IrisInput.tsv", @"C:\Users\slikm\Pulpit\IrisTarget2.tsv" }, 5, 0.8 ,true, 1, 0.5);
 // 4 8 4 2 1
 neuralNetwork.Run();
 Console.ReadKey();
